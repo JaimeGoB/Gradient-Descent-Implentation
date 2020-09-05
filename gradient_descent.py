c659e895e528b892ba8c8db2c08c2533726535f0
JaimeGoB <josba93@gmail.com>
2020-09-04 19:37:03 -0500

    Able to read data from github

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,3 +1,13 @@
 import os
 import pandas as pd
+import numpy as np
+
+#reading file from url
+url = 'https://raw.githubusercontent.com/JaimeGoB/Data-Sets/master/data_instanbul.csv'
+stocks = pd.read_csv(url)
+
+#checking for missing values or NaN
+stocks.isna().sum()
+
+stocks.head()
 
