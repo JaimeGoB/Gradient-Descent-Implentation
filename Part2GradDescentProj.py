301904952f69a92dc061fa885b836f4e8291c47f
calebcaptain1 <calebcaptain1@gmail.com>
2020-09-15 00:18:10 -0500

    Add files via upload

--- Part2GradDescentProj.py
+++ Part2GradDescentProj.py
@@ -1,6 +1,11 @@
 #This is the Standard Linear Regresssion Implimentation
-
+from sklearn.linear_model import LinearRegression
 from sklearn import metrics
+
+lm = LinearRegression()
+lm.fit(X_train, Y_train)
+predictions = lm.predict(X_test)
+
 print('MAE:', metrics.mean_absolute_error(Y_test, predictions))
 print('MSE:', metrics.mean_squared_error(Y_test, predictions))
 print('RMSE:', np.sqrt(metrics.mean_squared_error(Y_test, predictions)))
@@ -17,7 +22,7 @@ reg = make_pipeline(StandardScaler(), SGDRegressor(max_iter=2000, tol=1e-3))
 reg.fit(X_train, Y_train)
 #Pipeline(steps=[('standardscaler', StandardScaler()), ('sgdregressor', SGDRegressor())])
 pred = reg.predict(X_test)
-print('MAE:', metrics.mean_absolute_error(Y_test, predictions))
-print('MSE:', metrics.mean_squared_error(Y_test, predictions))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(Y_test, predictions)))
+print('MAE:', metrics.mean_absolute_error(Y_test, pred))
+print('MSE:', metrics.mean_squared_error(Y_test, pred))
+print('RMSE:', np.sqrt(metrics.mean_squared_error(Y_test, pred)))
 print('R^2:', metrics.r2_score(Y_test, pred))
